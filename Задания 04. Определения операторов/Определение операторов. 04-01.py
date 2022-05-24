4ead8c138a2dcbb3817d54028c757309a24ad195
ValmTim <100551624+ValmTim@users.noreply.github.com>
2022-05-24 20:19:28 +0500

    Update Определение операторов. 04-01.py

--- Задания 04. Определения операторов/Определение операторов. 04-01.py
+++ Задания 04. Определения операторов/Определение операторов. 04-01.py
@@ -13,7 +13,7 @@ class FoodInfo(object):
 
     def get_carbohydrates(self):
         return self.carb
-
+#Формула нахождения калорийности
     def get_kcalories(self):
         return 4 * self.prot + 9 * self.fats + 4 * self.carb
 
@@ -21,10 +21,8 @@ class FoodInfo(object):
         return FoodInfo(self.prot + value.prot, self.fats + value.fats, self.carb + value.carb)
 
 
-
 food1 = FoodInfo(1, 2, 3)
 food2 = FoodInfo(10, 20, 30)
-
 food3 = food1 + food2
 
 print(food3.get_proteins(), food3.get_fats(),
